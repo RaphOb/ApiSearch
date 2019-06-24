d9dccd3cef6b9b0739e02e73ba5202b72844c1b2
Raph <obadia_r@etna-alternance.net>
2019-06-24 21:44:30 +0200

    fix somes issues ( get works add works)

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Elasticsearch\ClientBuilder;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -6,7 +7,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $url = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
     if (is_file($file)) {
         return false;
@@ -42,38 +43,39 @@ $app->run();
  * @param Response $response
  * @return array
  */
-function elasticQueries(Request $request, Response $response) {
+function elasticQueries(Request $request, Response $response)
+{
     $param = $request->getQueryParams();
     $querie = $param['queri'];
     $client = ClientBuilder::create()->build();
     $param = [
         'index' => 'youtube',
-        'type' => 'videos',
         'body' => [
             'query' => [
                 'query_string' => [
-                    'query' => $querie.'~'
+                    'query' => $querie . '~'
                 ]
             ]
         ]
     ];
-  return $response = $client->get($param);
+    return error_log(print_r(($response = $client->search($param))));
 }
 
 /**
  * @param Request $request
  * @param Response $response
  */
-function addVideoElasticData(Request $request, Response $response) {
+function addVideoElasticData(Request $request, Response $response)
+{
     $data = $request->getParsedBody();
     $id = $request->getAttribute('id');
     $client = ClientBuilder::create()->build();
 
     $param = [
         'index' => 'youtube',
-        'type' => 'videos',
         'id' => $id,
         'body' => [
+            'typeOf' => 'videos',
             'name' => $data['name']
         ]
     ];
@@ -84,10 +86,11 @@ function addVideoElasticData(Request $request, Response $response) {
  * @param Request $request
  * @param Response $response
  */
-function deleteVideoElasticData(Request $request,Response $response) {
+function deleteVideoElasticData(Request $request, Response $response)
+{
     $client = ClientBuilder::create()->build();
     $id = $request->getAttribute('id');
-    $param= [
+    $param = [
         'index' => 'youtube',
         'type' => 'videos',
         'id' => $id
@@ -100,15 +103,17 @@ function deleteVideoElasticData(Request $request,Response $response) {
  * @param Request $request
  * @param Response $response
  */
-function addUserElasticData(Request $request, Response $response) {
+function addUserElasticData(Request $request, Response $response)
+{
     $data = $request->getParsedBody();
     $id = $request->getAttribute('id');
     $client = ClientBuilder::create()->build();
     $param = [
         'index' => 'youtube',
-        'type' => 'users',
+        'type' => 'videos',
         'id' => $id,
         'body' => [
+            'type' => 'users',
             'username' => $data['username'],
             'email' => $data['email']
         ]
@@ -122,10 +127,11 @@ function addUserElasticData(Request $request, Response $response) {
  * @param Request $request
  * @param Response $response
  */
-function deleteUserElasticData(Request $request, Response $response) {
+function deleteUserElasticData(Request $request, Response $response)
+{
     $client = ClientBuilder::create()->build();
     $id = $request->getAttribute('id');
-    $param= [
+    $param = [
         'index' => 'youtube',
         'type' => 'videos',
         'id' => $id
@@ -138,7 +144,8 @@ function deleteUserElasticData(Request $request, Response $response) {
  * @param Request $request
  * @param Response $response
  */
-function updateUserElasticData(Request $request, Response $response) {
+function updateUserElasticData(Request $request, Response $response)
+{
     $client = ClientBuilder::create()->build();
     $id = $request->getAttribute('id');
     $data = $request->getParsedBody();
@@ -160,7 +167,8 @@ function updateUserElasticData(Request $request, Response $response) {
  * @param Request $request
  * @param Response $response
  */
-function updateVideoElasticData(Request $request, Response $response) {
+function updateVideoElasticData(Request $request, Response $response)
+{
     $client = ClientBuilder::create()->build();
     $id = $request->getAttribute('id');
     $data = $request->getParsedBody();
--- src/routes.php
+++ src/routes.php
@@ -15,13 +15,13 @@ return function (App $app) {
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
 
-    $app->group('api/', function() use ($app) {
+    $app->group('/api', function() use ($app) {
         $app->get('/queries', 'elasticQueries');
         $app->post('/video/{id}', 'addVideoElasticData');
         $app->post('/user/{id}', 'addUserElasticData');
         $app->delete('/user/{id}', 'deleteUserElasticData');
         $app->delete('/video/{id}', 'deleteVideoElasticData');
-        $app->put('/video/{id}', 'updateUserElasticData');
+        $app->put('/user/{id}', 'updateUserElasticData');
         $app->put('/video/{id}', 'updateVideoElasticData');
     });
 };
